7f4d7ca6447306d41ffee2e877ea503168deb4b6
vicky <lch82327@gmail.com>
2016-05-05 11:05:22 +0800

    change folder archi

---
+++ lib/shipyard_api.rb
@@ -0,0 +1,3 @@
+require_relative './shipyard_api/shipyard.rb'
+require_relative './shipyard_api/container.rb'
+require_relative './shipyard_api/config/shipyard_config.rb'
---
+++ lib/shipyard_api/version.rb
@@ -0,0 +1,5 @@
+# Versioning
+module ShipyardAPI
+  VERSION = '0.0.1'
+  DATE = '2016-05-04'
+end
